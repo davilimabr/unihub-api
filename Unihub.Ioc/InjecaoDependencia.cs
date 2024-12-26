7de64ce42dbd3979f1d444e5e4b7cf0ca301aac2
Davi lima <davilimamoura.360@gmail.com>
2024-12-26 15:25:43 -0300

    feat: adicao injecao de dependencia

---
+++ Unihub.Ioc/InjecaoDependencia.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using unihub_api.Infraestrutura.Configuracao;
+
+namespace unihub_api.Ioc
+{
+    public static class InjecaoDependencia
+    {
+        public static IServiceCollection AdicionarInfraestrutura(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+             options.UseMySql(
+                 configuration.GetConnectionString("BancoDeDados"),
+                 new MySqlServerVersion(new Version(8, 0, 21)),
+                 b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)
+                 ));
+
+            //services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetAssembly(typeof(GetClubHandler))));
+
+
+            //services.AddAutoMapper(typeof(DomainToDtoMappingProfile));
+
+            return services;
+        }
+    }
+}
